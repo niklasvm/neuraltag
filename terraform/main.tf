eb6952162967f7eb2253f8b472da42da7656e3f6
niklasvm <niklasvm@gmail.com>
2025-03-16 20:07:54 +0200

    add terraform tests

---
+++ terraform/main.tf
@@ -0,0 +1,124 @@
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0" # or a later version
+    }
+  }
+  backend "gcs" {
+    bucket = "strava-terraform-state"
+  }
+}
+
+
+
+provider "google" {
+  credentials = var.gcp_sa_key
+  project     = var.gcp_project_id
+  region      = var.gcp_region
+}
+
+
+
+resource "google_artifact_registry_repository" "default" {
+  location      = var.gcp_region
+  repository_id = "strava"
+  description   = "Docker repository"
+  format        = "DOCKER"
+}
+
+resource "google_cloud_run_v2_service" "default" {
+  name     = "strava"
+  location = var.gcp_region
+
+  template {
+
+    containers {
+      command = ["fastapi", "run", "./src/app/main.py"]
+      args    = ["--host", "0.0.0.0", "--port", "8080"]
+      image   = "${google_artifact_registry_repository.default.location}-docker.pkg.dev/${var.gcp_project_id}/${google_artifact_registry_repository.default.repository_id}/strava:latest"
+      resources {
+        cpu_idle          = true
+        startup_cpu_boost = true
+        limits = {
+          cpu    = "1000m"
+          memory = "512Mi"
+        }
+      }
+      env {
+        name  = "APPLICATION_URL"
+        value = "https://strava-983811446432.africa-south1.run.app"
+      }
+      env {
+        name = "STRAVA_CLIENT_ID"
+        value_source {
+          secret_key_ref {
+            secret  = "STRAVA_CLIENT_ID"
+            version = "latest"
+          }
+        }
+      }
+      env {
+        name = "STRAVA_CLIENT_SECRET"
+        value_source {
+          secret_key_ref {
+            secret  = "STRAVA_CLIENT_SECRET"
+            version = "latest"
+          }
+        }
+      }
+      env {
+        name = "PUSHBULLET_API_KEY"
+        value_source {
+          secret_key_ref {
+            secret  = "PUSHBULLET_API_KEY"
+            version = "latest"
+          }
+        }
+      }
+      env {
+        name = "GEMINI_API_KEY"
+        value_source {
+          secret_key_ref {
+            secret  = "GEMINI_API_KEY"
+            version = "latest"
+          }
+        }
+      }
+      env {
+        name = "STRAVA_VERIFY_TOKEN"
+        value_source {
+          secret_key_ref {
+            secret  = "STRAVA_VERIFY_TOKEN"
+            version = "latest"
+          }
+        }
+      }
+      env {
+        name = "POSTGRES_CONNECTION_STRING"
+        value_source {
+          secret_key_ref {
+            secret  = "POSTGRES_CONNECTION_STRING"
+            version = "latest"
+          }
+        }
+      }
+      env {
+        name = "ENCRYPTION_KEY"
+        value_source {
+          secret_key_ref {
+            secret  = "ENCRYPTION_KEY"
+            version = "latest"
+          }
+        }
+      }
+    }
+    scaling {
+      min_instance_count = 0
+      max_instance_count = 2
+    }
+
+  }
+  timeouts {}
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "gcp_project_id" {
+  type = string
+}
+
+variable "gcp_sa_key" {
+  type = string
+}
+
+variable "gcp_region" {
+  type = string
+}
