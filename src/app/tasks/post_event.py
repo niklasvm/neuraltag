a184ee41bfa0bb5df87dffd21f6e79cede8b9553
niklasvm <niklasvm@gmail.com>
2025-03-21 06:42:05 +0200

    add support for a user to unsubscribe

--- src/app/tasks/post_event.py
+++ src/app/tasks/post_event.py
@@ -1,5 +1,6 @@
 import logging
 
+from src.app.db.adapter import Database
 from src.app.schemas.webhook_post_request import WebhookPostRequest
 from src.workflows import rename_workflow  # Import your route modules
 from src.app.core.config import Settings
@@ -12,6 +13,8 @@ def process_post_event(content: WebhookPostRequest, settings: Settings):
     """
     Processes the Strava webhook event by renaming the activity.
     """
+
+    # handle activity creation and activity update events
     logger.info(f"Received webhook event: {content}")
     if (content.aspect_type == "create" and content.object_type == "activity") or (
         content.aspect_type == "update"
@@ -31,3 +34,20 @@ def process_post_event(content: WebhookPostRequest, settings: Settings):
             logger.exception(
                 f"Error renaming activity {activity_id} for athlete {athlete_id}:"
             )
+
+    # handle unsubscribe events
+
+    if content.object_type == "athlete" and content.aspect_type == "update":
+        if content.updates is not None and "authorized" in content.updates:
+            if content.updates.get("authorized") == "false":
+                logger.info(f"Received unsubscribe event: {content}")
+                # delete athlete from database
+
+                logger.info(f"Deleting athlete {content.owner_id} from database")
+                athlete_id = content.owner_id
+                db = Database(
+                    settings.postgres_connection_string,
+                    encryption_key=settings.encryption_key,
+                )
+                db.delete_user(athlete_id)
+                logger.info(f"Successfully deleted athlete {athlete_id} from database")
