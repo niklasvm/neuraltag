2be909da4d513399fa2a524e091b0889f37c7600
niklasvm <niklasvm@gmail.com>
2025-03-22 19:11:15 +0200

    don't allow query params

--- src/app/pages/home.py
+++ src/app/pages/home.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, HTTPException, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
@@ -8,4 +8,8 @@ templates = Jinja2Templates(directory="src/app/templates")
 
 @router.get("/", response_class=HTMLResponse)
 async def home(request: Request):
+    if request.query_params:
+        raise HTTPException(
+            status_code=400, detail="Query parameters are not supported."
+        )
     return templates.TemplateResponse(request, "index.html", {"title": "Home Page"})
