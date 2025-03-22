390365f38e927771643c62a2f8697e3a6c12d87f
Niklas von Maltzahn <niklasvm@gmail.com>
2025-03-22 16:30:36 +0200

    Bump cpu

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,7 +42,7 @@ resource "google_cloud_run_v2_service" "default" {
         cpu_idle          = true
         startup_cpu_boost = true
         limits = {
-          cpu    = "2000m"
+          cpu    = "4000m"
           memory = "1024Mi"
         }
       }
