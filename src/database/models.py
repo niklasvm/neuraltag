0911d32f38a9f8cd6ebab62f55fff9a641cc28aa
Niklas von Maltzahn <niklasvm@gmail.com>
2025-05-24 10:28:17 +0200

    add promp version

--- src/database/models.py
+++ src/database/models.py
@@ -55,6 +55,7 @@ class User(Base):
     activity = relationship(
         "Activity", back_populates="user", cascade="all, delete-orphan"
     )
+    prompt_version = Column(String, nullable=True)
 
 
 class Activity(Base):
